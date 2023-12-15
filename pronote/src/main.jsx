9be6baf69e55943746b16b7ab9c8b1dd6cc683c8
Max3796 <gurpreet.singh@kalvium.community>
2023-12-15 14:38:50 +0530

    Done

---
+++ pronote/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Editor from './components/Editor.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Editor />
+  </React.StrictMode>,
+)
